ae3d2090446474d0cea80e7ae771752e38476fc3
Rj-batista <r.batista@outlook.fr>
2022-02-06 00:40:58 +0100

    as we go

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -22,17 +22,19 @@ object SimpleAPP extends App {
       .read
       .option("header", true)
       .csv("src/main/data/%s" format str)
-      .withColumn("First Tooltip",col("First Tooltip").cast(FloatType))
     val size_df = tmp_df.columns.size
     size_df match{
       case 6 =>
-        tmp_df
-          .filter(tmp_df("Indicator")==="Ambient and household air pollution attributable death rate (per 100 000 population, age-standardized)")
-          .filter(tmp_df("Dim1")==="Male"||tmp_df("Dim1")==="Female")
+      tmp_df
+        .filter(tmp_df("Indicator")==="Ambient and household air pollution attributable death rate (per 100 000 population, age-standardized)")
+        .withColumn("Full_value",split(col("First Tooltip")," ").getItem(0))
+        .withColumn("Range_value",split(col("First Tooltip")," ").getItem(1))
+        .drop("First Tooltip")
       case _ =>
         val tmp_df_year=tmp_df
           .filter(tmp_df("Period")==="2016")
           .drop("Indicator")
+          .withColumn("First Tooltip",col("First Tooltip").cast(FloatType))
         tmp_df_year.withColumnRenamed("First Tooltip","%s" format str_2)
 
     }
@@ -61,14 +63,69 @@ object SimpleAPP extends App {
 
   }
 
-  // Requête sur les dataset
+  /**
+   * Cette partie du code comprend l'ensemble des reqûetes sur les différentes dataframes
+   * Le but est d'extraire un ensemble de résultats sur la situation sanitaire/environnementale des pays du monde
+   *
+   *
+   */
+
+  //-----------------------------------------------------------------------------------------------
+  // Le % de population avec l'accès à l'eau potable basique (Search engine)
+  //-----------------------------------------------------------------------------------------------
+  def drink_water(country:String,annee:String):DataFrame={
+    val basic_drink = spark
+      .read
+      .option("header", true)
+      .csv("src/main/data/basicDrinkingWaterServices.csv")
+      .withColumn("First Tooltip",col("First Tooltip").cast(FloatType))
+      .withColumnRenamed("First Tooltip","% least basic service" )
+      .drop("Indicator")
+    basic_drink
+      .filter(basic_drink("Location")===country && basic_drink("Period")===annee)
+  }
+  //drink_water("Albania","2003").show()
+
+    //-----------------------------------------------------------------------------------------------
+    // Le % de population avec des moyens de nettoyage des mains basiques à la maison en 2016
+    //-----------------------------------------------------------------------------------------------
   val basic_hand  = clean_data("basicHandWashing.csv","% basic handwashing")
   basic_hand
-    .filter(basic_hand("Dim1")==="Total")
+    .filter(basic_hand("Dim1")==="Total") // Les deux
     .orderBy(desc("% basic handwashing"))
-    .show()
+    //.show()
+  basic_hand
+    .filter(basic_hand("Dim1")==="Rural")// En zone rural
+    .orderBy(asc("% basic handwashing"))
+    //.show()
+  basic_hand
+    .filter(basic_hand("Dim1")==="Urban")// En zone ubraine
+    .orderBy(desc("% basic handwashing"))
+    //.show()
 
-  val basic_drink = clean_data("basicDrinkingWaterServices.csv","% least basic service")
+  //-----------------------------------------------------------------------------------------------
+  // Pour 100k habitant les morts attribués à la pollution de l'air,age-standardized (SQL Queries)
+  //-----------------------------------------------------------------------------------------------
+
+  val air_pollution= clean_data("airPollutionDeathRate.csv")
+  air_pollution.createOrReplaceTempView("air_pollution")
+  // Nombre de mort pour 100k d'habitant supérieur à 250
+  //spark.sql("SELECT * FROM air_pollution WHERE air_pollution.Full_value > 250").show()
+
+  // Nombre de mort maximum les deux sexes & Nombre de mort maximum Homme ou Femme
+    //spark.sql("SELECT Location, Dim2,MAX(Full_value) FROM air_pollution " +
+      //                "WHERE air_pollution.Dim1 = 'Both sexes' GROUP BY Location,Dim2").show()
+    //spark.sql("SELECT Location,Dim2,MAX(Full_value) FROM air_pollution " +
+      //                "WHERE air_pollution.Dim1 = 'Female' GROUP BY Location,Dim2").show()
+
+  //Nombre de mort total par groupe de maladie
+
+    spark.sql("SELECT ")
+
+
+
+
+
 
 
 
